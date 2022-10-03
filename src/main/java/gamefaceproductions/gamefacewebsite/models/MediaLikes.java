2fca7af38edf49a1aadcc544b524aad97c56e651
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-10-03 15:38:34 -0500

    media class, media comments, and media likes done

---
+++ src/main/java/gamefaceproductions/gamefacewebsite/models/MediaLikes.java
@@ -0,0 +1,30 @@
+package gamefaceproductions.gamefacewebsite.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "media_likes")
+public class MediaLikes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"gameMedia"})
+    private User user;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"gameMedia"})
+    private GameMedia gameMedia;
+
+    @Column(nullable = false, length = 100)
+    private Boolean liked;
+}
