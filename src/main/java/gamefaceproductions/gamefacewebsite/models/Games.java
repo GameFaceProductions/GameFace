4adce6774ff75381a0274777db42946f9e8d6654
Valeria <valeriareveles12@gmail.com>
2022-10-03 10:33:28 -0500

    completed Games class skeleton

---
+++ src/main/java/gamefaceproductions/gamefacewebsite/models/Games.java
@@ -0,0 +1,24 @@
+package gamefaceproductions.gamefacewebsite.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="games")
+
+public class Games {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String games;
+}
