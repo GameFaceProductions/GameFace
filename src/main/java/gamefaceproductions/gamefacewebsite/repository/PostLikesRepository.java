c1efdb07b3c6baff4dbf3e1cac5102ad0b1d5f32
Valeria <valeriareveles12@gmail.com>
2022-10-04 12:12:46 -0500

    completed PostLikes repo

--- src/main/java/gamefaceproductions/gamefacewebsite/repository/PostLikesRepository.java
+++ src/main/java/gamefaceproductions/gamefacewebsite/repository/PostLikesRepository.java
@@ -1,7 +1,8 @@
 package gamefaceproductions.gamefacewebsite.repository;
 
 import gamefaceproductions.gamefacewebsite.models.Post;
+import gamefaceproductions.gamefacewebsite.models.PostLikes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostLikesRepository extends JpaRepository<Post, Long>{
+public interface PostLikesRepository extends JpaRepository<PostLikes, Long>{
 }
