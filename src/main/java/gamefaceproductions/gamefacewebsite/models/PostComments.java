6cf899870ac370aa09ec69ee8c4108c5248cc166
Valeria <valeriareveles12@gmail.com>
2022-10-04 09:23:53 -0500

    fixed infinite recursion, need to further refactor JsonIgnoreProps to match schema

--- src/main/java/gamefaceproductions/gamefacewebsite/models/PostComments.java
+++ src/main/java/gamefaceproductions/gamefacewebsite/models/PostComments.java
@@ -24,7 +24,7 @@ public class PostComments {
     private User author;
 
     @ManyToOne
-    @JsonIgnoreProperties("author")
+    @JsonIgnoreProperties({"author", "posts"})
     private Post posts;
 
     @Column(nullable = false)
