55010ab8fc6b801b4193cb1252417e1558470a3f
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-10-07 11:13:58 -0500

    HomePage showing author and loggedInUser is correctly fetched

--- src/main/resources/static/js/views/HomePage.js
+++ src/main/resources/static/js/views/HomePage.js
@@ -1,21 +1,19 @@
-import {getUser, getHeaders, isLoggedIn} from "../auth.js";
+import { getUser, getHeaders, isLoggedIn } from "../auth.js";
 import createView from "../createView.js";
-
 
 let posts;
 let loggedInUser;
 
-
 export default function HomePage(props) {
-    loggedInUser = getUser();
+  loggedInUser = getUser();
 
-    const postsHTML = generatePostsHTML(props.posts);
-    posts = props.posts;
-    console.log(props);
+  const postsHTML = generatePostsHTML(props.posts);
+  posts = props.posts;
+  console.log(props);
 
-    const addPostHTML = generateAddPostHTML();
+  const addPostHTML = generateAddPostHTML();
 
-    return `
+  return `
         <header>
             <h1>Posts Page</h1>
         </header>
@@ -30,13 +28,13 @@ export default function HomePage(props) {
 }
 
 function generateAddPostHTML() {
-    let addHTML = ``;
+  let addHTML = ``;
 
-    if(!isLoggedIn()) {
-        return addHTML;
-    }
+  if (!isLoggedIn()) {
+    return addHTML;
+  }
 
-    addHTML = `<h3>Add a post</h3>
+  addHTML = `<h3>Add a post</h3>
             <form>
                 <div>
                     <label for="title">Title</label><br>
@@ -69,11 +67,11 @@ function generateAddPostHTML() {
                 <button data-id="0" id="savePost" name="savePost" type="button" class="my-button button btn-primary">Save Post</button>
             </form>`;
 
-    return addHTML;
+  return addHTML;
 }
 
 function generatePostsHTML(posts) {
-    let postsHTML = `
+  let postsHTML = `
         <table class="table">
         <thead>
         <tr>
@@ -85,194 +83,185 @@ function generatePostsHTML(posts) {
         </thead>
         <tbody>
     `;
-    for (let i = 0; i < posts.length; i++) {
-        const post = posts[i];
+  for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
 
-        let authorName = "";
-        if (post.user) {
-            authorName = post.user;
-        }
+    let authorName = "";
+    if (post.user) {
+      authorName = post.author.userName;
+    }
 
-
-        postsHTML += `<tr>
+    postsHTML += `<tr>
             <td>${post.title}</td>
             <td>${post.content}</td>
-            <td>${authorName}</td>
-<!--            <td><p id="time">${post.createdAt}</p></td>-->
+            <td>${post.author.userName}</td>
+            <td><p id="time">${post.createdAt}</p></td>
             `;
 
-        //only admins and author of post can edit/delete it
-        if (loggedInUser.role === 'ADMIN' || loggedInUser.user === post.user) {
-            postsHTML += `<td><button data-id=${post.id} class="btn btn-primary editPost">Edit</button></td>
+    console.log(loggedInUser.role);
+    console.log(loggedInUser.userName);
+    //only admins and author of post can edit/delete it
+    if (loggedInUser.role === "ADMIN" || loggedInUser.user === post.user) {
+      postsHTML += `<td><button data-id=${post.id} class="btn btn-primary editPost">Edit</button></td>
             <td><button data-id=${post.id} class="btn btn-danger deletePost">Delete</button></td>`;
-        } else {
-            postsHTML += `<td></td><td></td>`
-        }
+    } else {
+      postsHTML += `<td></td><td></td>`;
+    }
     postsHTML += `</tr>`;
-}
-    postsHTML += `</tbody></table>`;
-    return postsHTML;
+  }
+  postsHTML += `</tbody></table>`;
+  return postsHTML;
 }
 
 export function postSetup() {
-    setupSaveHandler();
-    setupEditHandlers();
-    setupDeleteHandlers();
-    setupValidationHandlers();
-    validateFields();
+  setupSaveHandler();
+  setupEditHandlers();
+  setupDeleteHandlers();
+  setupValidationHandlers();
+  validateFields();
 }
 
 function setupValidationHandlers() {
-    let input = document.querySelector("#title");
-    input.addEventListener("keyup", validateFields);
-    input = document.querySelector("#content");
-    input.addEventListener("keyup", validateFields);
+  let input = document.querySelector("#title");
+  input.addEventListener("keyup", validateFields);
+  input = document.querySelector("#content");
+  input.addEventListener("keyup", validateFields);
 }
 
 function validateFields() {
-    let isValid = true;
-    let input = document.querySelector("#title");
-    if (input.value.trim().length < 1) {
-        input.classList.add("is-invalid");
-        input.classList.remove("is-valid");
-        isValid = false;
-    } else {
-        input.classList.add("is-valid");
-        input.classList.remove("is-invalid");
-    }
+  let isValid = true;
+  let input = document.querySelector("#title");
+  if (input.value.trim().length < 1) {
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    isValid = false;
+  } else {
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+  }
 
-    input = document.querySelector("#content");
-    if (input.value.trim().length < 1) {
-        input.classList.add("is-invalid");
-        input.classList.remove("is-valid");
-        isValid = false;
-    } else {
-        input.classList.add("is-valid");
-        input.classList.remove("is-invalid");
-    }
+  input = document.querySelector("#content");
+  if (input.value.trim().length < 1) {
+    input.classList.add("is-invalid");
+    input.classList.remove("is-valid");
+    isValid = false;
+  } else {
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+  }
 
-    return isValid;
+  return isValid;
 }
 
 function setupEditHandlers() {
-    // target all delete buttons
-    const editButtons = document.querySelectorAll(".editPost");
-    // add click handler to all delete buttons
-    for (let i = 0; i < editButtons.length; i++) {
-        editButtons[i].addEventListener("click", function (event) {
-            // get the post id of the delete button
-            const postId = parseInt(this.getAttribute("data-id"));
-            loadPostIntoForm(postId);
-        });
-    }
+  // target all delete buttons
+  const editButtons = document.querySelectorAll(".editPost");
+  // add click handler to all delete buttons
+  for (let i = 0; i < editButtons.length; i++) {
+    editButtons[i].addEventListener("click", function (event) {
+      // get the post id of the delete button
+      const postId = parseInt(this.getAttribute("data-id"));
+      loadPostIntoForm(postId);
+    });
+  }
 }
 
 function loadPostIntoForm(postId) {
-    // go find the post in the posts data that matches postId
-    const post = fetchPostById(postId);
-    if (!post) {
-        console.log("did not find post for id " + postId);
-        return;
-    }
-    // load the post data into the form
-    const titleField = document.querySelector("#title");
-    const contentField = document.querySelector("#content");
-    titleField.value = post.title;
-    contentField.value = post.content;
-    validateFields();
-    const saveButton = document.querySelector("#savePost");
-    saveButton.setAttribute("data-id", postId);
+  // go find the post in the posts data that matches postId
+  const post = fetchPostById(postId);
+  if (!post) {
+    console.log("did not find post for id " + postId);
+    return;
+  }
+  // load the post data into the form
+  const titleField = document.querySelector("#title");
+  const contentField = document.querySelector("#content");
+  titleField.value = post.title;
+  contentField.value = post.content;
+  validateFields();
+  const saveButton = document.querySelector("#savePost");
+  saveButton.setAttribute("data-id", postId);
 }
-
-
 
 function fetchPostById(postId) {
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === postId) {
-            return posts[i];
-        }
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].id === postId) {
+      return posts[i];
     }
-    // didn't find it so return something falsy
-    return false;
+  }
+  // didn't find it so return something falsy
+  return false;
 }
-
-
 
 function setupDeleteHandlers() {
-    // target all delete buttons
-    const deleteButtons = document.querySelectorAll(".deletePost");
-    // add click handler to all delete buttons
-    for (let i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener("click", function (event) {
-            // get the post id of the delete button
-            const postId = this.getAttribute("data-id");
-            deletePost(postId);
-        });
-    }
+  // target all delete buttons
+  const deleteButtons = document.querySelectorAll(".deletePost");
+  // add click handler to all delete buttons
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", function (event) {
+      // get the post id of the delete button
+      const postId = this.getAttribute("data-id");
+      deletePost(postId);
+    });
+  }
 }
-
-
 
 function deletePost(postId) {
-    const request = {
-        method: "DELETE",
-        headers: getHeaders(),
+  const request = {
+    method: "DELETE",
+    headers: getHeaders(),
+  };
+  const url = POST_API_BASE_URL + `/${postId}`;
+  fetch(url, request).then(function (response) {
+    if (response.status !== 200) {
+      console.log("fetch returned bad status: " + response.status);
+      console.log(response.statusText);
+      return;
     }
-    const url = POST_API_BASE_URL + `/${postId}`;
-    fetch(url, request)
-        .then(function (response) {
-            if(response.status !== 200){
-                console.log("fetch returned bad status: " + response.status);
-                console.log(response.statusText);
-                return;
-            }
-            createView("/home");
-        })
+    createView("/home");
+  });
 }
-
 
 function setupSaveHandler() {
-    const saveButton = document.querySelector("#savePost");
-    saveButton.addEventListener("click", function (event) {
-        const postId = parseInt(this.getAttribute("data-id"));
-        savePost(postId);
-    });
+  const saveButton = document.querySelector("#savePost");
+  saveButton.addEventListener("click", function (event) {
+    const postId = parseInt(this.getAttribute("data-id"));
+    savePost(postId);
+  });
 }
 
+function savePost(postId) {
+  const titleField = document.querySelector("#title");
+  const contentField = document.querySelector("#content");
 
-function savePost(postId){
-    const titleField = document.querySelector("#title");
-    const contentField = document.querySelector("#content");
-
-    if(!validateFields()){
-        return;
+  if (!validateFields()) {
+    return;
+  }
+  const post = {
+    title: titleField.value,
+    content: contentField.value,
+  };
+  // make the request
+  const request = {
+    method: "POST",
+    headers: getHeaders(),
+    body: JSON.stringify(post),
+  };
+  let url = POST_API_BASE_URL;
+  // if we are updating a post, change the request and the url
+  if (postId > 0) {
+    request.method = "PUT";
+    url += `/${postId}`;
+  }
+  fetch(url, request).then(function (response) {
+    if (response.status !== 200) {
+      console.log("fetch returned bad status code: " + response.status);
+      console.log(response.statusText);
+      return;
     }
-    const post = {
-        title: titleField.value,
-        content: contentField.value,
-    }
-    // make the request
-    const request = {
-        method: "POST",
-        headers: getHeaders(),
-        body: JSON.stringify(post)
-    }
-    let url = POST_API_BASE_URL;
-    // if we are updating a post, change the request and the url
-    if (postId > 0) {
-        request.method = "PUT";
-        url += `/${postId}`;
-    }
-    fetch(url, request)
-        .then(function (response) {
-            if(response.status !== 200){
-                console.log("fetch returned bad status code: " + response.status);
-                console.log(response.statusText);
-                return;
-            }
-            // location.reload();
-            createView('/home');
-        })
+    // location.reload();
+    createView("/home");
+  });
 }
 
 //
