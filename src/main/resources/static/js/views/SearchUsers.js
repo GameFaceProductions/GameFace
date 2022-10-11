f64ec5ebd9bc83bab2d402bd2bee555e1632be94
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-10-11 12:45:05 -0500

    Working on search users. Shows users based on input value. Will show ALL users initially

--- src/main/resources/static/js/views/SearchUsers.js
+++ src/main/resources/static/js/views/SearchUsers.js
@@ -6,33 +6,54 @@ export default function searchUsersHTML(props) {
   console.log(user);
   return `
 <!-- html here -->
-    <div id="userListContainer" class="row">
-        ${makeUserCards(user)}
+    <div class="container mt-1">
+    <form>
+      <div class="text-center">
+        <label for="searchUserInput" class="form-label">Search Users</label>
+        <input type="search" class="form-control" placeholder="Search User" id="searchUserInput">
+      <button type="submit" id="searchUserSubmitBtn" class="btn btn-dark mt-1">Search</button>
+      <div>
+    </form>
+      <div id="userListContainer" class="row">
+      </div>
     </div>
     `;
-
-  function makeUserCards(users) {
-    let html = "";
-    users.forEach(function (user) {
-      html += makeUserCard(user);
-    });
-    return html;
-  }
-
-  function makeUserCard(user) {
-    return `
-    <div class="card col-3 h-100">
-      <div class="card-body">
-        <h5 class="card-title movieTitle text-center">${user.userName}</h5>
-        <h5 class="card-title movieTitle text-center">${user.email}</h5>
-        <p class="card-text"></p>
-      </div>
-      <div id="movieFoot">
-        <button class="delBtn btn mb-2" data-id="${user.id}"><i class="fa fa-trash"></i></button>
-        </div>
-      </div>
-`;
-  }
 }
 
-export function searchUsersJS() {}
+export function searchUsersJS() {
+  let submitBtn = document.getElementById("searchUserSubmitBtn");
+  let searchUsersPageContainer = document.getElementById("userListContainer");
+  submitBtn.addEventListener("click", showSearchedUsers);
+  //
+  function showSearchedUsers() {
+    searchUsersPageContainer.innerHTML = `${makeUserCards(user)}`;
+
+    function makeUserCards(users) {
+      let searchUserInput = document.getElementById("searchUserInput");
+      let html = "";
+      user.forEach(function (user) {
+        console.log(user.userName);
+        if (user.userName.includes(searchUserInput.value)) {
+          html += makeUserCard(user);
+        } else {
+          console.log(user.userName + " does not match search.");
+        }
+      });
+      return html;
+    }
+
+    function makeUserCard(user) {
+      return `
+    <div class="searchCards card col-md-3 m-1 p-2">
+      <div class="card-body">
+        <h5 class="card-title searchUsersTitle text-center">${user.userName}</h5>
+        <p class="card-text"></p>
+      </div>
+      <div id="searchUsersFoot">
+        <button class="delBtn btn mb-2" data-id="${user.id}">+ ADD USER</button>
+        </div>
+    </div>
+`;
+    }
+  }
+}
