3ee4080b02a7165d756208bd10753d6d7e38ec69
Valeria <valeriareveles12@gmail.com>
2022-10-13 16:14:05 -0500

    updated home format, changed title to nullable=true in Post class

--- src/main/java/gamefaceproductions/gamefacewebsite/models/Post.java
+++ src/main/java/gamefaceproductions/gamefacewebsite/models/Post.java
@@ -19,7 +19,7 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = true, length = 100)
     private String title;
 
     @Column(nullable = false, length = 1024)
