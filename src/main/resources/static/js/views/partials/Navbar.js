3769a47bcfd7f28b74b7153b3ab9469560310d80
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-10-21 10:28:57 -0500

    navbar refromat with search drop down and avatar

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -2,21 +2,46 @@ import { getUser, isLoggedIn } from "../../auth.js";
 
 export default function Navbar(props) {
   let navbar = `
-        <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg bg-light">
     `;
 
   if (isLoggedIn()) {
     let user = getUser();
     navbar += `
-    <div class="container-fluid">
-        <img onclick="window.location.href='/';" class="nav-logo" src="/images/1.jpg" alt="logo" width="60px;" height="60px" style="border-radius:50%">
-        <i onclick="window.location.href='/';"  class="bi bi-info-circle-fill nav-item no-italic"><small class="nav-text">About Us</small></i> 
-        <i onclick="window.location.href='/home';" class="bi bi-house-fill nav-item no-italic"  ><small class="nav-text">Home</small></i>
-        <i onclick="window.location.href='/profile';" class="bi bi-person-circle nav-item no-italic"><small class="nav-text">Profile</small></i>
-        <i onclick="window.location.href='/searchusers';" class="bi bi-search nav-item no-italic"><small class="nav-text">Search</small></i>
-        <i onclick="window.location.href='/logout';" class="bi bi-box-arrow-in-right nav-item no-italic"><small class="nav-text">Logout</small></i>
-        <text  style="font-family: 'Press Start 2P', serif;" class="float-end">${user.userName}</text>`;
-
+  <div class="container-fluid">
+<img onClick="window.location.href='/';" class="nav-logo" src="/images/1.jpg" alt="logo" width="60px;"
+       height="60px" style="border-radius:50%">    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li>
+          <i onClick="window.location.href='/';" class="bi bi-info-circle-fill nav-item no-italic"><small
+        class="nav-text">About Us</small></i>
+        </li>
+        <li>
+    <i onClick="window.location.href='/home';" class="bi bi-house-fill nav-item no-italic"><small
+        class="nav-text">Home</small></i>        </li>
+        <li>
+         <i onClick="window.location.href='/profile';" class="bi bi-person-circle nav-item no-italic"><small
+        class="nav-text">Profile</small></i>
+</li>
+        <li class="nav-item dropdown">
+          <a class="nav-link bi bi-search dropdown-toggle nav-text" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Search
+          </a>
+          <ul class="dropdown-menu">
+<i onClick="window.location.href='/searchusers';" class="bi bi-search no-italic"><small
+        class="nav-text">Search Users</small></i>            <li><hr class="dropdown-divider"></li>
+<i onClick="window.location.href='/searchgames';" class="bi bi-search no-italic"><small
+        class="nav-text">Search Games</small></i>          </ul>
+        </li>
+        <li><i onClick="window.location.href='/logout';" class="bi bi-box-arrow-in-right nav-item no-italic"><small
+        class="nav-text">Logout</small></i></li>
+      </ul>
+      <img src="${user.avatar_url}" class="navbar-avatar rounded-circle" referrerpolicy="no-referrer">
+    </div>
+`;
   } else {
     navbar += `
        <i onclick="window.location.href='/';"  class="bi bi-info-circle-fill nav-item no-italic"><small class="nav-text">About Us</small></i> 
